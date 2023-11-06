2f0477ab70fb7aebad15720cef56f2ba8a4bd1a7
DESKTOP-3FEQ971\wsatoh <wsatoh@s1.spiritek.co.jp>
2023-11-06 14:51:38 +0900

    入力されていなかったらボタン非活性

--- src/components/Tweet.vue
+++ src/components/Tweet.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref , computed } from "vue";
 
 const tweets = ref([
 	{ id: 0, description: "Hello!" },
@@ -24,16 +24,26 @@ const deleteTweet = (id: number):void => {
 		return t.id !== id
 	})
 }
+
+const isInputting = computed(() => {
+	if (inputtingDescription.value === ''){
+		return true
+	} else {
+		false
+	}
+})
 </script>
 
 <template>
 	<div class="container flex flex-col items-center mx-auto">
 		<h1 class="text-3xl mb-8">Tweeter</h1>
 		<div class="flex flex-col items-center bg-gray-100 p-8 w-1/2 rounded-md">
+			<p v-if="inputtingDescription === ''" class="mb-2 text-lg font-bold">Tweetを入力してください。</p>
 			<input type="text" class="p-2 border border-black" v-model="inputtingDescription">
-			<button @click="postTweet" class="px-6 py-2 bg-teal-400 text-white mt-4 rounded-md cursor-pointer hover:bg-teal-300 transition-all duration-300">post</button>
+			<button @click="postTweet" :disabled="isInputting" class="px-6 py-2 bg-teal-400 text-white mt-4 rounded-md cursor-pointer hover:bg-teal-300 transition-all duration-300">post</button>
 		</div>
 		<div class="mt-8">
+			<p v-if="tweets.length <= 0" class="text-center text-lg font-bold">Tweetが投稿されていません。</p>
 			<ul class=" px-0 p-2 w-96">
 				<li v-for="tweet in tweets" :key="tweet.id" class="list-none text-xl bg-slate-200 p-2 rounded-md flex justify-between items-center mb-4">
 					<span class="p-2">{{ tweet.description }}</span>
