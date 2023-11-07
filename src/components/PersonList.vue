5d8c916fad07ef6cd54e55dcbe1c64f86dc670bb
DESKTOP-3FEQ971\wsatoh <wsatoh@s1.spiritek.co.jp>
2023-11-07 15:54:57 +0900

    削除ボタン押下時にダイアログを表示

--- src/components/PersonList.vue
+++ src/components/PersonList.vue
@@ -9,9 +9,13 @@ type Props = {
 defineProps<Props>()
 
 const emit = defineEmits(['delete'])
-const deletePerson = (id: number) => {
+const deletePerson = (id: number , name: string):void => {
     console.log(id);
-    emit('delete' , id)
+    if (confirm(name + ' ' + 'を削除しますか？？')){
+        emit('delete' , id)
+    } else {
+        return
+    }
 }
 
 </script>
@@ -20,6 +24,6 @@ const deletePerson = (id: number) => {
     <li v-for="person in persons" :key="person.id" class=" bg-indigo-200 py-2 px-4 mb-4 rounded-md flex items-center justify-between md:w-128 w-96">
         <span>{{ person.name }} : </span>
         <span>age: {{ person.age }}</span>
-        <button @click="deletePerson(person.id)" class="bg-red-300 py-2 px-4 rounded-md transition-all duration-300 hover:bg-red-100">delete</button>
+        <button @click="deletePerson(person.id , person.name)" class="bg-red-300 py-2 px-4 rounded-md transition-all duration-300 hover:bg-red-100">delete</button>
     </li>
 </template>
